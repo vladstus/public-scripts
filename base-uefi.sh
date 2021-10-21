6331bb44c7d8c84e7bd0a61966216734a77430c8
vladislavstus <stus.vlad@nixsolutions.com>
2021-10-21 18:15:47 +0300

    updated sudoers file permission

--- base-uefi.sh
+++ base-uefi.sh
@@ -13,28 +13,27 @@ echo "gaazhag" >>/etc/hostname
   echo "127.0.1.1 gaazhag.localdomain gaazhag"
 } >>/etc/hosts
 echo root:nosferatu | chpasswd
-
+### Install Grub
 pacman -S grub efibootmgr dhcpcd dhclient networkmanager
 
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-
+### Configure sudoers
+rm -r /etc/sudoers
 cp /public-scrips/sudoers /etc/sudoers
+chmod 440 /etc/sudoers
 useradd -m -G wheel -s /bin/bash gaazhag
 echo gaazhag:nosferatu | chpasswd
 
-## You can add xorg to the installation packages, I usually add it at the DE or WM install script
-## You can remove the tlp package if you are installing on a desktop or vm
-#
+### Enable services
+systemctl enable NetworkManager
+
+### Install drivers
+pacman -Syu nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings lib32-vulkan-icd-loader lib32-opencl-nvidia opencl-nvidia libxnvctrl
 #pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflectorbase-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 #
-## pacman -S --noconfirm xf86-video-amdgpu
-#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-#
-#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-#grub-mkconfig -o /boot/grub/grub.cfg
-#
-#systemctl enable NetworkManager
+
+
 #systemctl enable bluetooth
 #systemctl enable cups.service
 #systemctl enable sshd
