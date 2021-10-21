1b2509d7aa3c4b115ec2afbd21a728b61307f3b0
vladislavstus <stus.vlad@nixsolutions.com>
2021-10-21 18:47:20 +0300

    try installing more packages

--- base-uefi.sh
+++ base-uefi.sh
@@ -27,30 +27,27 @@ chmod 440 /etc/sudoers
 useradd -m -G wheel -s /bin/bash gaazhag
 echo gaazhag:nosferatu | chpasswd
 
-### Enable services
-systemctl enable NetworkManager
-
 ### Install drivers
 pacman -Syu nvidia-dkms nvidia-utils nvidia-settings opencl-nvidia libxnvctrl
-#pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflectorbase-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-#
+
+### Install other stuff
+pacman -S network-manager-applet dialog wpa_supplicant mtools reflectorbase-devel avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 
-#systemctl enable bluetooth
-#systemctl enable cups.service
-#systemctl enable sshd
-#systemctl enable avahi-daemon
-#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-#systemctl enable reflector.timer
-#systemctl enable fstrim.timer
+### Enable services
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
 #systemctl enable libvirtd
-#systemctl enable firewalld
-#systemctl enable acpid
-#
-#useradd -m gaazhag
-#echo gaazhag:nosferatu | chpasswd
+systemctl enable firewalld
+systemctl enable acpid
+
+
 #usermod -aG libvirt gaazhag
-#
-#echo "gaazhag ALL=(ALL) ALL" >>/etc/sudoers1.d/gaazhag
-#
-#printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+printf "\e[1;32mDone! Type exit, umount -R /mnt and reboot.\e[0m"
