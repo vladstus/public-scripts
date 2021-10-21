102878f101d7226b54a440107efbca920a4fe2fd
vladislavstus <stus.vlad@nixsolutions.com>
2021-10-21 16:52:13 +0300

    make script shorter

--- base-uefi.sh
+++ base-uefi.sh
@@ -14,33 +14,37 @@ echo "gaazhag" >>/etc/hostname
 } >>/etc/hosts
 echo root:nosferatu | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
+pacman -S grub efibootmgr dhcpcd dhclient networkmanager
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflectorbase-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-
-# pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable cups.service
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
-
-useradd -m gaazhag
-echo gaazhag:nosferatu | chpasswd
-usermod -aG libvirt gaazhag
-
-echo "gaazhag ALL=(ALL) ALL" >>/etc/sudoers.d/gaazhag
-
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+## You can add xorg to the installation packages, I usually add it at the DE or WM install script
+## You can remove the tlp package if you are installing on a desktop or vm
+#
+#pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflectorbase-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+#
+## pacman -S --noconfirm xf86-video-amdgpu
+#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+#
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+#grub-mkconfig -o /boot/grub/grub.cfg
+#
+#systemctl enable NetworkManager
+#systemctl enable bluetooth
+#systemctl enable cups.service
+#systemctl enable sshd
+#systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+#systemctl enable reflector.timer
+#systemctl enable fstrim.timer
+#systemctl enable libvirtd
+#systemctl enable firewalld
+#systemctl enable acpid
+#
+#useradd -m gaazhag
+#echo gaazhag:nosferatu | chpasswd
+#usermod -aG libvirt gaazhag
+#
+#echo "gaazhag ALL=(ALL) ALL" >>/etc/sudoers.d/gaazhag
+#
+#printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
