a6d17c4f90b54dc08161ee0b419fb88327611b45
vladislavstus <stus.vlad@nixsolutions.com>
2021-10-21 16:24:07 +0300

    update pacman

--- base-uefi.sh
+++ base-uefi.sh
@@ -17,11 +17,11 @@ echo root:nosferatu | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools \
- reflectorbase-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils \
- bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion \
- openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq \
- vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools
+pacman -S reflectorbase-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils
+pacman -S bluez bluez-utils cups hplip alsa-utils pulseaudio pavucontrol bash-completion
+pacman -S openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq
+pacman -S vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
