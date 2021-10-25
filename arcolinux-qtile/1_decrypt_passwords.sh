c40e2635dba12a4f76b4bc3f85e23627030310c3
vladstus <vlad.stus@gmail.com>
2021-10-25 18:21:22 +0300

    update file permissions

--- arcolinux-qtile/1_decrypt_passwords.sh
+++ arcolinux-qtile/1_decrypt_passwords.sh
@@ -2,5 +2,5 @@
 
 yes | sudo pacman -S age
 age --decrypt -o ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore.bak
-cat .xignore ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore
+cat ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore
 echo "Login into Chrome, 1P and github"
