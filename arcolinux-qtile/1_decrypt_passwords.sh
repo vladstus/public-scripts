1db9cf693c4296c32a5fc383f0ae44e8bb007919
vladstus <vlad.stus@gmail.com>
2021-10-25 17:14:24 +0300

    update pwd

--- arcolinux-qtile/1_decrypt_passwords.sh
+++ arcolinux-qtile/1_decrypt_passwords.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 yes | sudo pacman -S age
-age --decrypt -o .xignore .xignore.bak && cat .xignore
+age --decrypt -o ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore.bak
+cat .xignore ~/Projects/Config_Files/public-scripts/arcolinux-qtile/.xignore
 echo "Login into Chrome, 1P and github"
