df3cdcecfe21f1eb2c545415dac695695429e314
vladstus <vlad.stus@gmail.com>
2021-10-25 16:57:30 +0300

    added more scripts

---
+++ arcolinux-qtile/5_load_zsh_plugins.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/autojump
+cd ~/.oh-my-zsh/custom/plugins/autojump || exit
+python ./install.py
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+yes | sudo pacman -S python-pip vifm neovim
+
+
+pip install virtualenvwrapper
+
+curl -s "https://get.sdkman.io" | bash
+source "/home/gaazhag/.sdkman/bin/sdkman-init.sh"
