d8f6b251334d5b917417b938fc524ef456065743
vladstus <vlad.stus@gmail.com>
2021-10-26 17:42:57 +0300

    changed folder for zsh plugins

--- arcolinux-qtile/5_load_zsh_plugins.sh
+++ arcolinux-qtile/5_load_zsh_plugins.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/you-should-use
-git clone https://github.com/supercrabtree/k "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:=~/.oh-my-zsh/custom}"/plugins/zsh-completions
+sudo git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM:-/usr/share/oh-my-zsh/custom}"/plugins/you-should-use
+sudo git clone https://github.com/supercrabtree/k "${ZSH_CUSTOM:-/usr/share/oh-my-zsh/custom}"/plugins/k
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-/usr/share/oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-/usr/share/oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:=/usr/share/oh-my-zsh/custom}"/plugins/zsh-completions
 
-git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/autojump
-cd ~/.oh-my-zsh/custom/plugins/autojump || exit
+sudo git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:=/usr/share/oh-my-zsh/custom}/plugins/autojump
+cd /usr/share/oh-my-zsh/custom/plugins/autojump || exit
 python ./install.py
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
